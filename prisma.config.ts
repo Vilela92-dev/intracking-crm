294fd6e4980b47077535edb58f5a1e652924fe53
Vilela92-dev <contatointracking@gmail.com>
2026-04-08 19:48:18 -0300

    chore: migration para postgress (neon) concluída

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig, env } from '@prisma/config';
+import { defineConfig } from '@prisma/config';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+
+// Força a leitura do arquivo .env informando o caminho absoluto
+dotenv.config({ path: resolve(process.cwd(), '.env') });
 
 export default defineConfig({
+  earlyAccess: true,
   datasource: {
-    url: env('DATABASE_URL'),
+    // Usamos o process.env e adicionamos um fallback para debug
+    url: process.env.DATABASE_URL
   },
 });
