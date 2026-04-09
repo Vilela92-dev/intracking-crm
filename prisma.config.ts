e0d3caa8ab50c836e807973ddc0de35f3bcc5819
Vilela92-dev <contatointracking@gmail.com>
2026-04-09 19:08:45 -0300

    fix: remove invalid prisma config property

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
-import { defineConfig } from '@prisma/config';
-import * as dotenv from 'dotenv';
-import { resolve } from 'path';
-
+import { defineConfig } from 'prisma';
 
 export default defineConfig({
-  earlyAccess: true,
-  datasource: {
-    // Usamos o process.env e adicionamos um fallback para debug
-    url: process.env.DATABASE_URL
-  },
+  schema: './prisma/schema.prisma',
 });
